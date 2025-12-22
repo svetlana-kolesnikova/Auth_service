07ceff676fd27e501e4cb5701878eaedf0dd994f
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-12-22 21:28:34 +0300

    add models.py, admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
+# users/admin.py
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "phone", "invite_code", "used_invite", "is_staff")
+    search_fields = ("phone", "invite_code")
