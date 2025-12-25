fdc319a90043fa48eef29f01c5ca52d59dcf7159
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-12-25 22:28:37 +0300

    fixes urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path
 
-urlpatterns = [
+from __future__ import annotations
+
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+
+urlpatterns: list = [
+    # Django admin
     path("admin/", admin.site.urls),
+    # Users app
+    path("", include("users.urls")),
+    # OpenAPI schema
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Swagger UI
+    path(
+        "api/docs/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    # ReDoc UI
+    path(
+        "api/docs/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
