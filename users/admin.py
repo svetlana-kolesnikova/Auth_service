70a0c04d73566b0b1f792632a21b3ae4cfbb6047
svetlana-kolesnikova <sv.ps1286@gmail.com>
2026-01-09 20:16:23 +0300

    Final fixes_2

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,6 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    "Админка для модели User"
+    """Админка для модели User"""
     list_display = ("id", "phone", "invite_code", "used_invite", "is_staff")
     search_fields = ("phone", "invite_code")
--- users/urls.py
+++ users/urls.py
@@ -7,16 +7,13 @@ from .views import (ActivateInviteView, LoginTemplateView, LogoutView, PhoneAuth
 app_name = "users"
 
 urlpatterns: list = [
-    # -------------------------
     # HTML Template Views
-    # -------------------------
     path("login/", LoginTemplateView.as_view(), name="login"),
     path("verify/", VerifyCodeTemplateView.as_view(), name="verify"),
     path("profile/", ProfileTemplateView.as_view(), name="profile"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    # -------------------------
+
     # API Views
-    # -------------------------
     path("api/phone-auth/", PhoneAuthView.as_view(), name="api_phone_auth"),
     path("api/phone-verify/", PhoneVerifyView.as_view(), name="api_phone_verify"),
     path("api/profile/", ProfileView.as_view(), name="api_profile"),
